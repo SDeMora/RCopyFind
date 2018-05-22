e5df2eeb5a72aa5cf865d8297a54475e607787c1
Stephanie DeMora <38257602+SDeMora@users.noreply.github.com>
2018-05-22 10:42:38 -0700

    Update RCopy-class.R

--- R/RCopy-class.R
+++ R/RCopy-class.R
@@ -3,11 +3,4 @@
 # Class: RCopy #
 ################
 
-
-# Set initial class #
-################
-# Class: RCopy #
-################
-
-
 setClass("RCopy", slots=c(data = "data.frame"), contains=c("data.frame"))
