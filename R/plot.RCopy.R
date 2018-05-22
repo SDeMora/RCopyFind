8682f4803fc1a15060ca9bd4d8aef8f920d0da85
Stephanie DeMora <38257602+SDeMora@users.noreply.github.com>
2018-05-22 10:30:01 -0700

    Update plot.RCopy.R

--- R/plot.RCopy.R
+++ R/plot.RCopy.R
@@ -6,44 +6,31 @@
 # trim = logical, default = FALSE, TRUE trims count only 25 => word overlap; percent > 0
 # ... other plot arguments
 
-plot.RCopy <- function(x, count=TRUE, trim=FALSE,...) {
-  
-  if (count) { # Overall Match Counts #
-    if (trim) { # Trim to Matches greater than 25
-      x@data <- x@data[x@data$Overall.Match_count >= 25,]
-    } # close trim
-    
-    # Sort Data for plotting display #
-    x@data <- x@data[rev(order(x@data$Perfect.Match_count)),]
-    xvar <- x@data[,7] # Count Number column
-    labs <- x@data[,10] # Labels column#
-    
-    # Initiate Plot
-    dotchart(xvar,
-             xlab="Word Similarity",
-             bty="n",
-             main = "Textual Similarity to Model Legislation",
-             labels=labs,
-             ...)
-    
-  } else { # Overall Percent, not counts #
-    
-    if (trim) { # Trimmed to greater than 0% match
-      x@data <- x@data[x@data$Overall.Match_percent > 0,]
-    } # close trim
-    
-    # Sort Data for plotting display #
-    x@data <- x@data[rev(order(x@data$Overall.Match_percent)),]
-    xvar <- x@data[,8] # Percent Column
-    labs <- x@data[,10] # Labels Column #
-    
-    # Initiate Plot #
-    dotchart(xvar,
-             xlab="Percent Word Similarity",
-             bty="n",
-             main = "Textual Similarity to Model Legislation",
-             labels=labs,
-             ...)
-  } # close else
-  
+plot.RCopy <-function (x, count = TRUE, trim = FALSE, ...) 
+{
+    if (count) {
+        if (trim) {
+            x@data <- x@data[x@data$Overall.Match_count >= 25, 
+                ]
+        }
+        x@data <- x@data[rev(order(x@data$Perfect.Match_count)), 
+            ]
+        xvar <- x@data[, 7]
+        labs <- x@data[, 10]
+        dotchart(xvar, xlab = "Word Similarity", bty = "n", main = "Textual Similarity to Model Legislation", 
+            labels = labs, ...)
+    }
+    else {
+        if (trim) {
+            x@data <- x@data[x@data$Overall.Match_percent > 0, 
+                ]
+        }
+        x@data <- x@data[rev(order(x@data$Overall.Match_percent)), 
+            ]
+        xvar <- x@data[, 8]
+        labs <- x@data[, 10]
+        dotchart(xvar, xlab = "Percent Word Similarity", bty = "n", 
+            main = "Textual Similarity to Model Legislation", 
+            labels = labs, ...)
+    }
 }
