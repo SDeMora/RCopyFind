af9d8ecfdae1cdc0fe00eb3ccc98778e8bebdb88
Stephanie DeMora <38257602+SDeMora@users.noreply.github.com>
2018-05-22 10:42:54 -0700

    Update file_name.R

--- R/file_name.R
+++ R/file_name.R
@@ -5,13 +5,3 @@
 # dir = string/character of directory where matches.html resides
 file_name <- function(dir) {
   paste0("file://", dir, "/matches.html")
-
-#######################
-# Function: file_name #
-#######################
-
-# dir = string/character of directory where matches.html resides
-file_name <- function(dir) {
-  paste0("file://", dir, "/matches.html")
-
-}
