08c31329d80a2a9c0a975ba3f0257a9c375b32f1
Steph DeMora <sdemo001@ucr.edu>
2018-05-30 20:51:04 -0700

    Update scripter .r and .Rd

--- R/scripter.R
+++ R/scripter.R
@@ -1,24 +1,27 @@
-scripter<-function(a=200,b="C:\\Users\\xheli\\Desktop\\CReport",
-                   c=1,ca="C:\\Users\\xheli\\Desktop\\ALEC Model Leg.txt",
-                   d=2,db="C:\\Users\\xheli\\Desktop\\txt files",
-                   e=8,f=6,g=20,h=9,i=75,j=0,k=1,l=1,m=1,n=1,o=1,p=1,q=2){
-  arg1 <- paste("Documents,",a, sep="")
-  arg2 <- paste("ReportFolder,",b,sep="")
-  arg3 <- paste("Document,",c,",",ca,sep="")
-  arg4 <- paste("Document,",d,",",db,sep="")
-  arg5 <- paste("PhraseLength,",e,sep="")
-  arg6 <- paste("WordThreshold,",f,sep="")
-  arg7 <- paste("SkipLength,",g,sep="")
-  arg8 <- paste("MismatchTolerance,",h,sep="")
-  arg9 <- paste("MismatchPercentage,",i,sep="")
-  arg10 <- paste("BriefReport,",j,sep="")
-  arg11 <- paste("IgnoreCase,",k,sep="")
-  arg12 <- paste("IgnoreNumber,",l,sep="")
-  arg13 <- paste("IgnoreOuterPunctuation,",m,sep="")
-  arg14 <- paste("IgnorePunctuation,",n,sep="")
-  arg15 <- paste("SkipLongWords,",o,sep="")
+scripter<-function(Documents=200,ReportFolder="C:\\Users\\xheli\\Desktop\\CReport",
+                   Document1=1,Document1_loc="C:\\Users\\xheli\\Desktop\\ALEC Model Leg.txt",
+                   Document2=2,Document2_loc="C:\\Users\\xheli\\Desktop\\txt files",
+                   PhraseLength=8,WordThreshold=6,SkipLength=20,MismatchTolerance=9,
+                   MismatchPercentage=75,BriefReport=0,IgnoreCase=1,IgnoreNumber=1,
+                   IgnoreOuterPunctuation=1,IgnorePunctuation=1,
+                   SkipLongWords=1,Compare1=1,Compare2=2){
+  arg1 <- paste("Documents,",Documents, sep="")
+  arg2 <- paste("ReportFolder,",ReportFolder,sep="")
+  arg3 <- paste("Document,",Document1,",",Document1_loc,sep="")
+  arg4 <- paste("Document,",Document2,",",Document2_loc,sep="")
+  arg5 <- paste("PhraseLength,",PhraseLength,sep="")
+  arg6 <- paste("WordThreshold,",WordThreshold,sep="")
+  arg7 <- paste("SkipLength,",SkipLength,sep="")
+  arg8 <- paste("MismatchTolerance,",MismatchTolerance,sep="")
+  arg9 <- paste("MismatchPercentage,",MismatchPercentage,sep="")
+  arg10 <- paste("BriefReport,",BriefReport,sep="")
+  arg11 <- paste("IgnoreCase,",IgnoreCase,sep="")
+  arg12 <- paste("IgnoreNumber,",IgnoreNumber,sep="")
+  arg13 <- paste("IgnoreOuterPunctuation,",IgnoreOuterPunctuation,sep="")
+  arg14 <- paste("IgnorePunctuation,",IgnorePunctuation,sep="")
+  arg15 <- paste("SkipLongWords,",SkipLongWords,sep="")
   arg16<- paste("Locale,English")
-  arg17 <- paste("Compare,",p,",",q,sep="")
+  arg17 <- paste("Compare,",Compare1,",",Compare2,sep="")
   script<-paste(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,
                 arg10,arg11,arg12,arg13,arg14,arg15,arg16,
                 "PrepareForComparisons",
