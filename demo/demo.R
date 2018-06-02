92ebcd08104e4467652444969bdac80327c2f9cc
Steph DeMora <sdemo001@ucr.edu>
2018-06-02 16:49:43 -0700

    Update examples and Demo

---
+++ demo/demo.R
@@ -0,0 +1,27 @@
+##1.) Create a folder in your working directory called "CReport".
+
+##2.) Save directory as "OldWD":
+OldWD<-paste(getwd(),"/CReport", sep="")
+
+##3.) Get path to .exe in R library examples:
+#CopyFind Program
+DIRECT<-system.file("CopyFind", package="RCopyFind")
+#Example Text for "Document1_loc"
+Doc1<-system.file("ALEC Model Leg.txt", package="RCopyFind")
+#Example Text for "Document2_loc"
+Doc2<-system.file("txt files", package="RCopyFind")
+
+##4.) Re-set your directory (LEAVE "CReport" Wherever your initial directory (oldWD) was.).
+setwd(DIRECT)
+
+#Double check:
+getwd()
+
+
+##5.) Save a script to THAT (above) folder:
+script<-scripter(ReportFolder = OldWD, Document1_loc = DOc1, Document2_loc = Doc2)
+#Write it out:
+write.table(script,"script.txt", col.names = FALSE, row.names = FALSE, quote = FALSE)
+
+#Run it.
+run_copyfind(cleanup = T,"script")
